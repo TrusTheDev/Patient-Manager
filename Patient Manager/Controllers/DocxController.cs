c0ff54c6d9a5edd6a47486355af4cdd3bde5d6e8
TrusTheDev <ivanbordeira2015@hotmail.com>
2025-10-06 18:39:57 -0300

    project structure, DocX integration and implementation

---
+++ Patient Manager/Controllers/DocxController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Words.NET;
+
+namespace Patient_Manager.Controllers
+{
+    internal class DocxController
+    {
+        public static DocX RepairDocx(String rutaDocx)
+        {
+            string rutaTemporal = "doc_temp.docx";
+
+            // Descomprimir temporalmente
+            ZipFile.ExtractToDirectory(rutaDocx, "doc_temp");
+
+            // Reemplazar en todos los archivos XML
+            foreach (var xml in Directory.GetFiles("doc_temp", "*.xml", SearchOption.AllDirectories))
+            {
+                string contenido = File.ReadAllText(xml);
+                contenido = contenido.Replace("w:jc w:val=\"start\"", "w:jc w:val=\"left\"");
+                File.WriteAllText(xml, contenido);
+            }
+
+            // Volver a comprimir
+            if (File.Exists(rutaTemporal)) File.Delete(rutaTemporal);
+            ZipFile.CreateFromDirectory("doc_temp", rutaTemporal);
+
+            // Limpiar carpeta temporal
+            Directory.Delete("doc_temp", true);
+
+            // Cargar el documento reparado
+            var doc = Xceed.Words.NET.DocX.Load(rutaTemporal);
+            return doc;
+        }
+    }
+}
--- Patient Manager/Form1.cs
+++ Patient Manager/Form1.cs
@@ -3,25 +3,42 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xceed.Words.NET;
 using static Patient_Manager.Controllers.DateController;
+using static Patient_Manager.Controllers.DocxController;
+using System.IO.Compression;
+
+
 
 namespace Patient_Manager
 {
     public partial class Form1 : Form
     {
+        String PatientDocPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\PatientDocs\Septiembre.docx";
         public Form1()
         {
             InitializeComponent();
             label1.Text = GetDate();
 
+
+                DocX document = RepairDocx(PatientDocPath);
+                foreach (var fila in document.Tables[0].Rows)
+                {
+                    string textoCelda = fila.Cells[0].Paragraphs[0].Text;
+                    Console.WriteLine(textoCelda);
+                }
+            
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             
         }
 
